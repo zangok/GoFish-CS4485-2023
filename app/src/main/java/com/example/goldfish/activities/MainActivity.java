b10382201dabb6a416315386c31d0a315fe334cc
happy230711 <130940242+happy230711@users.noreply.github.com>
2023-10-20 14:10:04 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/goldfish/activities/MainActivity.java
+++ app/src/main/java/com/example/goldfish/activities/MainActivity.java
@@ -1,17 +1,15 @@
 package com.example.goldfish.activities;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.ComponentActivity;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.airbnb.lottie.LottieDrawable;
-import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ComponentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +20,13 @@ public class MainActivity extends AppCompatActivity {
         String appName = getString(R.string.app_name); // Gets the app_name string
         String startGameText = getString(R.string.start_game); // Gets the start_game string
 
-        // Button startGameButton = findViewById(R.id.startGameButton);
-        // startGameButton.setText(startGameText);
-
         // Find the LottieAnimationView
         LottieAnimationView animationView = findViewById(R.id.animationView);
         // Set animation resource
         animationView.setAnimation(R.raw.animation);
         // Start the animation
         animationView.playAnimation();
-        // loop the animation (for infinite loop)
+        // Optionally, loop the animation (for an infinite loop)
         animationView.setRepeatCount(LottieDrawable.INFINITE);
 
         animationView.setScaleX(4.0f); // Adjust the scale as needed
@@ -42,16 +37,8 @@ public class MainActivity extends AppCompatActivity {
         // Get the TextView by its ID
         TextView titleTextView = findViewById(R.id.titleTextView);
 
-        // Set the title with an emoji
+        // Set the text with an emoji
         titleTextView.setText("Go\uD83C\uDCCFFish");
 
-        Button rulesButton = findViewById(R.id.rulesButton);
+        Button
 
-        rulesButton.setOnClickListener(view -> {
-            // Handle the button click
-            Snackbar.make(view, "Display rules", Snackbar.LENGTH_SHORT).show();
-        });
-
-        Button settingsButton = findViewById(R.id.settingsButton);
-    }
-}
