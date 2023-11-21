961fdad38a18097ee0519193a47754019f56d4ea
Asritha <98362128+akorrapati1@users.noreply.github.com>
2023-11-20 22:47:11 -0600

    Update GameActivity.java

--- app/src/main/java/com/example/gofish/activities/GameActivity.java
+++ app/src/main/java/com/example/gofish/activities/GameActivity.java
@@ -4,27 +4,37 @@ public class GameActivity {
     private List<Player> players;
     private CardDeck cardDeck;
     private Player currentPlayer;
-
+    
+    //Constructor to initialize players, card deck, and start the game
     public GameActivity(List<String> playerNames) {
         initializePlayers(playerNames);
         cardDeck = new CardDeck();
         currentPlayer = players.get(0);
         startGame();
     }
-
+    
+    //Initialize players based on given names
     private void initializePlayers(List<String> playerNames) {
         players = new ArrayList<>();
         for (String name : playerNames) {
             players.add(new Player(name));
         }
     }
-
+    
+    //Start the game by shuffling the deck and dealing initial cards
     private void startGame() {
         cardDeck.shuffle();
+        if(cardDeck.isEmpty()) {
+            throw new IllegalStateException("Deck is empty.");
+        }
         dealInitialCards();
     }
-
+    
+    //Deal initial cards to all players
     private void dealInitialCards() {
+        if(cardDeck.isEmpty()) {
+            throw new IllegalStateException("Deck is empty.");
+        }
         int numPlayers = players.size();
         int numCardsToDeal = (numPlayers >= 4) ? 5 : 7;
 
@@ -34,8 +44,12 @@ public class GameActivity {
             }
         }
     }
-
+    
+    //Play a turn in the game, asking the given target player for a specific rank
     public boolean playTurn(Player targetPlayer, String rankToAsk) {
+        if(targetPlayer == null || rankToAsk == null) {
+            throw new IllegalArgumentException("targetPlayer and rankToAsk are null.");
+        }
         if (currentPlayer == targetPlayer) {
             return false; // Current player cannot ask themselves.
         }
@@ -56,7 +70,8 @@ public class GameActivity {
             }
         }
     }
-
+    
+    //Check for sets in a player's hand based on the given rank
     private void checkForSets(Player player, String rank) {
         int count = player.countRankCards(rank);
         if (count == 4) {
@@ -65,16 +80,19 @@ public class GameActivity {
             player.addToScore(1);
         }
     }
-
+    
+    //Switch to the next player in the game
     private void switchToNextPlayer() {
         int currentPlayerIndex = players.indexOf(currentPlayer);
         currentPlayer = players.get((currentPlayerIndex + 1) % players.size());
     }
-
+    
+    //Check if the game is over
     public boolean isGameOver() {
         return cardDeck.isEmpty() || allSetsCollected();
     }
-
+    
+    //Check if all sets have been collected by players
     private boolean allSetsCollected() {
         for (Player player : players) {
             if (player.getScore() < 13) {
@@ -83,7 +101,8 @@ public class GameActivity {
         }
         return true;
     }
-
+    
+    //Get the winner of the game
     public Player getWinner() {
         Player winner = players.get(0);
         for (Player player : players) {
