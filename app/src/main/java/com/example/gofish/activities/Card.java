9afbd502a125c9fff579369523f77ec94c70d79a
Asritha <98362128+akorrapati1@users.noreply.github.com>
2023-11-20 22:45:39 -0600

    Update Card.java

--- app/src/main/java/com/example/gofish/activities/Card.java
+++ app/src/main/java/com/example/gofish/activities/Card.java
@@ -1,37 +1,53 @@
 package com.example.gofish.activities;
 
 public class Card {
+    //Instance variables to store the rank and suit of the card
     private String rank;
     private String suit;
-
+    
+    //Constructor for creating a Card with a specific rank and suit
     public Card(String rank, String suit) {
+        //Error handling for invalid rank or suit
+        if(rank == null) {
+            throw new IllegalArgumentException("Card rank cannot be null.");
+        }
+        if(suit == null) {
+            throw new IllegalArgumentException("Card suit cannot be null.");
+        }
+        
+        //Initialize the Card
         this.rank = rank;
         this.suit = suit;
     }
-
+    
+    //Get the rank of the card
     public String getRank() {
         return rank;
     }
-
+    
+    //Update the rank of the card
     public void setRank(String rank) {
         this.rank = rank;
     }
-
+    
+    //Get the suit of the card
     public String getSuit() {
         return suit;
     }
-
+    
+    //Update the suit of the card
     public void setSuit(String suit) {
         this.suit = suit;
     }
-
+    
+    //Check if two cards are equal
     public boolean equals(Card otherCard) {
         return this.rank.equals(otherCard.getRank()) && this.suit.equals(otherCard.getSuit());
     }
-
+    
+    //Get the string representation of the card
     @Override
     public String toString() {
         return rank + " of " + suit;
     }
 }
-
