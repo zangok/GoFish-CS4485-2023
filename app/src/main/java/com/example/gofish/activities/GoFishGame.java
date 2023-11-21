1595740d25a770f9f4750e5808e5e9d947366faa
Asritha <98362128+akorrapati1@users.noreply.github.com>
2023-11-20 22:47:59 -0600

    Update GoFishGame.java

--- app/src/main/java/com/example/gofish/activities/GoFishGame.java
+++ app/src/main/java/com/example/gofish/activities/GoFishGame.java
@@ -6,20 +6,29 @@ public class GoFishGame {
   private List<Player> players;
   private CardDeck cardDeck;
   private Player currentPlayer;
-
+    
+  //Constructor to initialize players, card deck, and start the game
   public GoFishGame(List<Player> players) {
+    if(players == null || players.isEmpty()) {
+        throw new IllegalArgumentException("Players list cannot be null or empty.");
+    }
     this.players = players;
     this.cardDeck = new CardDeck();
     this.currentPlayer = players.get(0);
     startGame();
   }
-
+    
+  //Start the game by shuffling the deck and dealing initial cards
   private void startGame() {
     cardDeck.shuffle();
     dealCards();
   }
-
+    
+  //Deal initial cards to all players
   private void dealCards() {
+    if(cardDeck.isEmpty()) {
+        throw new IllegalStateException("Deck is empty.");
+    }
     int numCardsToDeal;
     if(players.size() >= 4) {
       numCardsToDeal = 5;
@@ -34,11 +43,13 @@ public class GoFishGame {
     }
   }
 
+  //Switch to the next player
   private void switchToNextPlayer() {
     int currentPlayerIndex = players.indexOf(currentPlayer);
     currentPlayer = players.get((currentPlayerIndex + 1) % players.size());
   }
-
+    
+  //Check if the game is over based on an empty card deck
   public boolean gameOver() {
     return cardDeck.isEmpty();
   }
