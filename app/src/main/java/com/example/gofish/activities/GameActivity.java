c7d1d176cdef0bcef7b686813ed3a356a74c4b94
happy230711 <130940242+happy230711@users.noreply.github.com>
2023-12-01 14:06:16 -0600

    Update GameActivity.java

--- app/src/main/java/com/example/gofish/activities/GameActivity.java
+++ app/src/main/java/com/example/gofish/activities/GameActivity.java
@@ -1,115 +1,181 @@
 package com.example.gofish.activities;
 
-public class GameActivity {
-    private List<Player> players;
-    private CardDeck cardDeck;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.List;
+
+public class GameActivity extends AppCompatActivity {
+    private Game goFishGame;
     private Player currentPlayer;
-    
-    //Constructor to initialize players, card deck, and start the game
-    public GameActivity(List<String> playerNames) {
-        initializePlayers(playerNames);
-        cardDeck = new CardDeck();
-        currentPlayer = players.get(0);
-        startGame();
-    }
-    
-    //Initialize players based on given names
-    private void initializePlayers(List<String> playerNames) {
-        players = new ArrayList<>();
-        for (String name : playerNames) {
-            players.add(new Player(name));
-        }
-    }
-    
-    //Start the game by shuffling the deck and dealing initial cards
-    private void startGame() {
-        cardDeck.shuffle();
-        if(cardDeck.isEmpty()) {
-            throw new IllegalStateException("Deck is empty.");
-        }
-        dealInitialCards();
-    }
-    
-    //Deal initial cards to all players
-    private void dealInitialCards() {
-        if(cardDeck.isEmpty()) {
-            throw new IllegalStateException("Deck is empty.");
-        }
-        int numPlayers = players.size();
-        int numCardsToDeal = (numPlayers >= 4) ? 5 : 7;
+    private Player opponentPlayer;
+    private MediaPlayer clickSound;
+    private RecyclerView currentPlayerRecyclerView, opponentPlayerRecyclerView;
+    private CardAdapter currentPlayerAdapter, opponentPlayerAdapter;
+    private TextView currentPlayerScoreTextView, opponentPlayerScoreTextView;
 
-        for (int i = 0; i < numCardsToDeal; i++) {
-            for (Player player : players) {
-                player.addToHand(cardDeck.drawCard());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        // Find the LottieAnimationView
+        LottieAnimationView animationView = findViewById(R.id.animationView);
+
+        // Set animation resource
+        animationView.setAnimation(R.raw.animation);
+
+        // Start the animation
+        animationView.playAnimation();
+
+        // Optionally, loop the animation (for infinite loop)
+        animationView.setRepeatCount(LottieDrawable.INFINITE);
+
+        // Adjust the scale as needed
+        animationView.setScaleX(4.0f);
+        animationView.setScaleY(4.0f);
+
+        // Initialize the MediaPlayer with the button click sound
+        clickSound = MediaPlayer.create(this, R.raw.bubblesound);
+
+        currentPlayerRecyclerView = findViewById(R.id.currentPlayerRecyclerView);
+        opponentPlayerRecyclerView = findViewById(R.id.opponentPlayerRecyclerView);
+        currentPlayerScoreTextView = findViewById(R.id.currentPlayerScoreTextView);
+        opponentPlayerScoreTextView = findViewById(R.id.opponentPlayerScoreTextView);
+
+        currentPlayerRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        opponentPlayerRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        initializeGame();
+
+        Button playTurnButton = findViewById(R.id.playTurnButton);
+        playTurnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playTurn();
             }
+        });
+
+        // Set OnClickListener for the homeButton
+        Button homeButton = findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Play the button click sound
+                playButtonClickSound();
+
+                // Create an Intent to go back to MainActivity
+                Intent intent = new Intent(GameActivity.this, MainActivity.class);
+
+                // Start the MainActivity
+                startActivity(intent);
+
+                // Finish the current activity (GameActivity) to remove it from the back stack
+                finish();
+            }
+        });
+    }
+
+
+    private void initializeGame() {
+        List<String> playerNames = new ArrayList<>();
+        playerNames.add("Player 1");
+        playerNames.add("Player 2");
+
+        goFishGame = new Game(playerNames);
+
+        currentPlayer = goFishGame.getCurrentPlayer();
+        opponentPlayer = getOpponentPlayer(); // Use the helper method to get the opponent player
+
+        currentPlayerAdapter = new CardAdapter(currentPlayer.getHand());
+        opponentPlayerAdapter = new CardAdapter(opponentPlayer.getHand());
+
+        currentPlayerRecyclerView.setAdapter(currentPlayerAdapter);
+        opponentPlayerRecyclerView.setAdapter(opponentPlayerAdapter);
+
+        updateScores();
+        updateUI();
+    }
+
+    // Add this helper method to get the opponent player
+    private Player getOpponentPlayer() {
+        int currentPlayerIndex = goFishGame.getPlayers().indexOf(currentPlayer);
+        int opponentPlayerIndex = (currentPlayerIndex + 1) % goFishGame.getPlayers().size();
+        return goFishGame.getPlayers().get(opponentPlayerIndex).copy(); // Copy the opponent player instance
+    }
+
+    private void playTurn() {
+        // Implement the logic for a turn
+        // For simplicity, let's assume the user always asks for the first rank in their hand
+        String rankToAsk = currentPlayer.getHand().get(0).getRank();
+        boolean successfulTurn = goFishGame.playTurn(opponentPlayer, rankToAsk);
+
+        if (successfulTurn) {
+            updateScores();
+            updateUI();
         }
     }
-    
-    //Play a turn in the game, asking the given target player for a specific rank
-    public boolean playTurn(Player targetPlayer, String rankToAsk) {
-        if(targetPlayer == null || rankToAsk == null) {
-            throw new IllegalArgumentException("targetPlayer and rankToAsk are null.");
-        }
-        if (currentPlayer == targetPlayer) {
-            return false; // Current player cannot ask themselves.
-        }
 
-        List<Card> cardsToReceive = targetPlayer.transferCards(rankToAsk);
-        if (!cardsToReceive.isEmpty()) {
-            currentPlayer.addToHand(cardsToReceive);
-            checkForSets(currentPlayer, rankToAsk);
-            return true; // Player successfully received cards.
+    private void updateScores() {
+        currentPlayerScoreTextView.setText("Score P1: " + currentPlayer.getScore());
+        opponentPlayerScoreTextView.setText("Score P2: " + getOpponentPlayer().getScore());
+    }
+
+    private void updateUI() {
+        currentPlayerAdapter.notifyDataSetChanged();
+        opponentPlayerAdapter.notifyDataSetChanged();
+
+        if (goFishGame.isGameOver()) {
+            showGameOverDialog();
         } else {
-            Card drawnCard = cardDeck.drawCard();
-            currentPlayer.addToHand(drawnCard);
-            if (drawnCard.getRank().equals(rankToAsk)) {
-                return true; // Player drew a matching card and gets another turn.
-            } else {
-                switchToNextPlayer();
-                return false; // Player drew a different card, and it's the next player's turn.
-            }
+            switchPlayers();
         }
     }
-    
-    //Check for sets in a player's hand based on the given rank
-    private void checkForSets(Player player, String rank) {
-        int count = player.countRankCards(rank);
-        if (count == 4) {
-            // Player has collected a set, remove these cards from their hand and increase the score.
-            player.removeRankCards(rank);
-            player.addToScore(1);
+
+    private void switchPlayers() {
+        currentPlayer = getOpponentPlayer();
+    }
+
+    private void showGameOverDialog() {
+        // Implement logic to show a dialog or navigate to the end game screen
+        // For now, you can display a simple toast message
+        Toast.makeText(this, "Game Over! Winner: " + goFishGame.getWinner().getName(), Toast.LENGTH_SHORT).show();
+    }
+    // Method to play the button click sound
+    private void playButtonClickSound() {
+        if (clickSound != null) {
+            clickSound.start();
         }
     }
-    
-    //Switch to the next player in the game
-    private void switchToNextPlayer() {
-        int currentPlayerIndex = players.indexOf(currentPlayer);
-        currentPlayer = players.get((currentPlayerIndex + 1) % players.size());
-    }
-    
-    //Check if the game is over
-    public boolean isGameOver() {
-        return cardDeck.isEmpty() || allSetsCollected();
-    }
-    
-    //Check if all sets have been collected by players
-    private boolean allSetsCollected() {
-        for (Player player : players) {
-            if (player.getScore() < 13) {
-                return false;
-            }
+
+    // Override onDestroy to release the MediaPlayer when the activity is destroyed
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (clickSound != null) {
+            clickSound.release();
+            clickSound = null;
         }
-        return true;
-    }
-    
-    //Get the winner of the game
-    public Player getWinner() {
-        Player winner = players.get(0);
-        for (Player player : players) {
-            if (player.getScore() > winner.getScore()) {
-                winner = player;
-            }
-        }
-        return winner;
     }
 }
